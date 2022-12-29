d53e996af28568d6f5bb116e15cb959b73580927
DanDaMan23 <danlawrcons.gmail.com>
2022-12-29 15:53:44 -0600

    Basic Post Serializer

--- social_media_app/models.py
+++ social_media_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
 # Create your models here.
+
+
+class Post(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    user = models.CharField(max_length=20, null=False)
--- social_media_app/serializers.py
+++ social_media_app/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from social_media_app.models import Post
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,18 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+class PostSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    content = serializers.CharField(required=True)
+    user = serializers.CharField(required=True)
+
+    def create(self, validated_data):
+        """
+        Create and return a new 'Post' instance, given the validated data.
+        """
+        return Post.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.content = validated_data.get('content', instance.content)
+        return instance
