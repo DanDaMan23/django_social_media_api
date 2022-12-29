fe11bd0f7a7500a4cb0a2c818cdbfe195fa3ab33
DanDaMan23 <danlawrcons.gmail.com>
2022-12-29 14:15:13 -0600

    Created basic API for admin users

---
+++ social_media_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework import routers
+from social_media_app import views
+
+router = routers.DefaultRouter()
+router.register(r'user', views.UserViewSet)
+router.register(r'group', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionaly, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
--- social_media_app/views.py
+++ social_media_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, permissions
+from social_media_app.serializers import UserSerializer, GroupSerializer
 
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API end point that allows users to be viewed or edited
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows group ot be viewed or edited
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
