e7d99b18d105805baf9e9a205cdb3cf975d18648
DanDaMan23 <danlawrcons.gmail.com>
2022-12-30 14:42:55 -0600

    Created posts list and post detail api

--- social_media_app/serializers.py
+++ social_media_app/serializers.py
@@ -14,17 +14,24 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         model = Group
         fields = ['url', 'name']
 
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    content = serializers.CharField(required=True)
-    user = serializers.CharField(required=True)
 
-    def create(self, validated_data):
-        """
-        Create and return a new 'Post' instance, given the validated data.
-        """
-        return Post.objects.create(**validated_data)
+# class PostSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     content = serializers.CharField(required=True)
+#     user = serializers.CharField(required=True)
 
-    def update(self, instance, validated_data):
-        instance.content = validated_data.get('content', instance.content)
-        return instance
+#     def create(self, validated_data):
+#         """
+#         Create and return a new 'Post' instance, given the validated data.
+#         """
+#         return Post.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         instance.content = validated_data.get('content', instance.content)
+#         return instance
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'content', 'user']
--- social_media_app/urls.py
+++ social_media_app/urls.py
@@ -5,10 +5,13 @@ from social_media_app import views
 router = routers.DefaultRouter()
 router.register(r'user', views.UserViewSet)
 router.register(r'group', views.GroupViewSet)
+# router.register(r'post', views.post_list)
 
 # Wire up our API using automatic URL routing.
 # Additionaly, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('posts/', views.post_list),  # type: ignore
+    path('posts/<int:pk>', views.post_detail),  # type: ignore
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
