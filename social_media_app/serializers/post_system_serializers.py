996cae4a04d9f5b34ccea61de51cf3f304b09f68
DanDaMan23 <danlawrcons.gmail.com>
2023-01-29 21:35:14 -0600

    Created username property

--- social_media_app/serializers/post_system_serializers.py
+++ social_media_app/serializers/post_system_serializers.py
@@ -4,16 +4,19 @@ from social_media_app.models import Post, Comment
 
 
 class PostSerializer(ModelSerializer):
+    username = ReadOnlyField()
 
     class Meta:
         model = Post
-        fields = ['id', "content", "user", "comments"]
+        fields = ['id', "content", "comments", "username"]
         read_only_fields = ["comments"]
         user = ReadOnlyField(source='user.url')
 
 
 class CommentSerializer(ModelSerializer):
+    username = ReadOnlyField()
+
     class Meta:
         model = Comment
-        fields = ["content", "post", "user"]
+        fields = ["content", "post", "username"]
         read_only_fields = ['user']
